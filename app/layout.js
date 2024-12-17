48ba4c18ee927561c80a88e44f12df2733a16ad4
Amey patil <103939656+Ameycoder8853@users.noreply.github.com>
2024-12-17 20:34:50 +0530

    Add files via upload

---
+++ app/layout.js
@@ -0,0 +1,29 @@
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import Provider from "./provider";
+import {Outfit} from "next/font/google"
+
+
+const outfit = Outfit({subsets:['latin']})
+
+
+export const metadata = {
+  title: "Videomatics AI",
+  description: "Your Own Video Generator AI",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <ClerkProvider>
+    <html lang="en">
+      <body
+        className={outfit.className}
+      >
+        <Provider>
+        {children}
+        </Provider>
+      </body>
+    </html>
+    </ClerkProvider>
+  );
+}
