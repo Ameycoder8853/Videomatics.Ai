9d7148a0a57f67a4dc32d2f6b567e7a51f995258
Amey patil <103939656+Ameycoder8853@users.noreply.github.com>
2024-12-22 20:04:42 +0530

    Update Header.jsx

--- app/dashboard/_components/Header.jsx
+++ app/dashboard/_components/Header.jsx
@@ -1,13 +1,15 @@
-"use client";
-import { UserButton } from "@clerk/nextjs";
-import Image from "next/image";
-import Link from "next/link"; // Import Link for navigation
-import React from "react";
-import SideNav from "./SideNav"; // Import SideNav Component
+import { UserButton } from '@clerk/nextjs';
+import Image from 'next/image';
+import React, { useContext } from 'react';
+import { UserDetailContext } from '../../_context/UserDetailContext';
+import Link from 'next/link';
+import SideNav from './SideNav';
 
 const Header = () => {
+  const { userDetail } = useContext(UserDetailContext);
+
   return (
-    <header className="sticky top-0 z-50 bg-white dark:bg-gray-800 shadow-md p-5 px-8 flex items-center justify-between h-20">
+    <div className="sticky top-0 z-50 bg-white dark:bg-gray-800 shadow-md p-5 px-8 flex items-center justify-between h-20">
       {/* Left Section: Logo and Heading */}
       <div className="flex items-center gap-6">
         <Link href="/" aria-label="Home"> {/* Clickable Logo */}
@@ -15,14 +17,18 @@ const Header = () => {
         </Link>
         <Link href="/" aria-label="Home"> {/* Clickable Heading */}
           <h2 className="font-bold text-2xl text-gray-900 dark:text-gray-100 cursor-pointer">
-            Videomatics AI
+            Videomatic AI
           </h2>
         </Link>
+        <div className="flex gap-2">
+          <Image src={'/rupee.png'} width={20} height={20}/>
+          <h2 className="text-gray-900 dark:text-gray-100">credits:{userDetail?.credits}</h2>
+        </div>
       </div>
 
       {/* Right Section: SideNav */}
-      <SideNav />
-    </header>
+        <SideNav />
+    </div>
   );
 };
 
