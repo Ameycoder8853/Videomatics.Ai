c2a0ce201dee8323958b030ed0113aa10bdba433
Amey patil <103939656+Ameycoder8853@users.noreply.github.com>
2025-01-01 19:30:31 +0530

    Update schema.js

--- app/schema.js
+++ app/schema.js
@@ -1,20 +1,22 @@
-import { pgTable, serial, varchar, boolean, json} from "drizzle-orm/pg-core";
+import { pgTable, serial, varchar, boolean, json, integer} from "drizzle-orm/pg-core";
 
 export const Users =pgTable('users',{
     id:serial('id').primaryKey(),
     name:varchar('name').notNull(),
     email:varchar('email').notNull(),
     imageUrl:varchar('imageUrl'),
-    subscription:boolean('subscription').default(false)
-
+    subscription:boolean('subscription').default(false),
+    subscriptionEndDate: varchar('subscriptionEndDate'),
+    credits:integer('credits').default(30)
 })
 
 
 export const VideoData = pgTable('videoData', {
-    id:serial('id').primaryKey(),
-    script:json('script').notNull(),
-    audioFileUrl:varchar('audioFileUrl').notNull(), // No `notNull` to avoid insertion issues
-    captions:json('captions').notNull(),
-    imageList:varchar('imageList').array(), // Changed to JSON for storing as an array
-    createdBy:varchar('createdBy').notNull()
+    id: serial('id').primaryKey(),
+    script: json('script').notNull(),
+    audioFileUrl: varchar('audioFileUrl').notNull(), // No `notNull` to avoid insertion issues
+    captions: json('captions').notNull(),
+    imageList: varchar('imageList').array(), // Changed to JSON for storing as an array
+    createdBy: varchar('createdBy').notNull(),
 });
+
